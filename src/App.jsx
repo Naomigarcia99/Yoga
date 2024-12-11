585197a7ad966acc4febd85f9d7dfd90a0fbf705
Naomi García Robles <naomigarciarobles21@gmail.com>
2024-12-11 12:15:33 +0100

    Print users CRUD

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import Graphics from "./pages/graphics";
 import Calendar from "./pages/calendar";
 import Navbar from "./components/navbar";
 import { Routes, Route } from "react-router-dom";
+import { UserProvider } from "./context/UserContext";
 
 function App() {
-
   return (
-    <>
+    <UserProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -17,7 +17,7 @@ function App() {
         <Route path="/calendar" element={<Calendar />} />
         <Route path="/graphics" element={<Graphics />} />
       </Routes>
-    </>
+    </UserProvider>
   );
 }
 
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
+import UserList from "../components/UserList";
 
 export default function home() {
   return (
-    <div>home</div>
-  )
+    <>
+      <UserList />
+    </>
+  );
 }
